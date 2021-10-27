e6a26e1f6ebbd2fb38d4e7241165938db8898378
estesca11 <cjo7897@gmail.com>
2021-10-27 17:37:42 +0900

    stopwatch

--- exercise.js
+++ exercise.js
@@ -1,13 +1,2 @@
-function a(str) {
-    console.log(str);
-    return;
-}
-
-a(null == undefined);
-a(1 == "1");
-a("0xff"==255);
-a(true == 1);
-a(true == "1");
-a(new String("a") == "a");
-a(new Number(2) == 2);
-a([2] == 2);
+var name =prompt("A");
+console.log()
