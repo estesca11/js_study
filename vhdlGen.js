8a3941b70509ffacf5fea78d8b12d44cde219d35
estesca11 <cjo7897@pukyong.ac.kr>
2021-11-18 10:14:36 +0900

    server

--- vhdlGen.js
+++ vhdlGen.js
@@ -1,6 +1,6 @@
 //0~16 binary generator
-var bin = 1;
+let bin = 1;
 for (let i = 0; i < 16; i++) {
-    console.log(`WHEN "${bin.toString(2).padStart(16,'0')}" => seg <= "";`);
-    bin=bin<<1;
+    console.log(`WHEN "${bin.toString(2).padStart(16, '0')}" => seg <= "";`);
+    bin = bin << 1;
 }
